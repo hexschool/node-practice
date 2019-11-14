fdf7132f98aabb9687b11eb9803021b6fdd9d587
hsiangfeng <loveyou520babytw@gmail.com>
2019-11-14 19:58:24 +0800

    修正 Router 標題

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '六角 Git 特訓班' });
 });
 
 module.exports = router;
